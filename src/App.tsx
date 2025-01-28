341ecbf32329cdf24d1d206623d0b0196aa77cb3
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-01-28 12:47:19 -0300

    styles: theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,19 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "sonner";
 
+import { ThemeProvider } from "./components/theme/theme-provider";
 import { Router } from "./router";
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <Toaster richColors />
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <Toaster richColors />
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
