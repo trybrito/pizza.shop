c65a83edb7afd759f3fcd9a8be7de893788b850f
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-01-28 13:35:11 -0300

    styles: starts orders page

--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
 import { Dashboard } from "./pages/app/dashboard";
+import { Orders } from "./pages/app/orders";
 import { SignIn } from "./pages/auth/sign-in";
 import { SignUp } from "./pages/auth/sign-up";
 
@@ -11,7 +12,7 @@ export function Router() {
     <Routes>
       <Route path="/" element={<AppLayout />}>
         <Route path="/" element={<Dashboard />} />
-        {/* <Route path="/orders" element={<Dashboard />} /> */}
+        <Route path="/orders" element={<Orders />} />
       </Route>
 
       <Route path="/" element={<AuthLayout />}>
