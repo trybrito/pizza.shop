e0cf88e9b9adcbb2f6afce9b7dc916404eb47e57
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-01-30 20:47:33 -0300

    chore: configs axios and react-query

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,10 +1,12 @@
 import "./global.css";
 
+import { QueryClientProvider } from "@tanstack/react-query";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "sonner";
 
 import { ThemeProvider } from "./components/theme/theme-provider";
+import { queryClient } from "./lib/react-query";
 import { Router } from "./router";
 
 export function App() {
@@ -13,9 +15,11 @@ export function App() {
       <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
         <Helmet titleTemplate="%s | pizza.shop" />
         <Toaster richColors />
-        <BrowserRouter>
-          <Router />
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   );
