4b29d7575cf3351ed443f6b09e90a4c0e9edec5f
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-01-30 21:05:11 -0300

    Revert "bugfix: fixes import file naming error"
    This reverts commit 69e6d796383609924a702933624504de5f294e0e.

--- web/src/api/sign-in.ts
+++
@@ -1,9 +0,0 @@
-import { api } from "@/lib/axios";
-
-interface SignInBody {
-  email: string;
-}
-
-export async function signIn({ email }: SignInBody) {
-  await api.post("/authenticate", { email });
-}
--- web/src/pages/auth/sign-in.tsx
+++ web/src/pages/auth/sign-in.tsx
@@ -1,12 +1,10 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useMutation } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import { toast } from "sonner";
 import { z } from "zod";
 
-import { signIn } from "@/api/sign-in";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -26,13 +24,9 @@ export function SignIn() {
     resolver: zodResolver(signInFormSchema),
   });
 
-  const { mutateAsync: authenticate } = useMutation({
-    mutationFn: signIn,
-  });
-
   async function handleSignIn(data: SignInFormInputs) {
     try {
-      await authenticate({ email: data.email });
+      await new Promise((resolve) => setTimeout(resolve, 2000));
 
       toast.success("Enviamos um link de autenticação para seu e-mail!", {
         action: {
