43db6180c73b72588553c5dfaed5cbf3f280b585
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-02-01 05:16:29 -0300

    styles: order details skeleton loading

--- web/src/pages/app/orders/order-details.tsx
+++ web/src/pages/app/orders/order-details.tsx
@@ -21,6 +21,8 @@ import {
 } from "@/components/ui/table";
 import { formatToBRL } from "@/utils/formatter";
 
+import { OrderDetailsSkeleton } from "./order-details-skeleton";
+
 interface OrderDetailsProps {
   orderId: string;
   open: boolean;
@@ -40,7 +42,7 @@ export function OrderDetails({ orderId, open }: OrderDetailsProps) {
         <DialogDescription>Detalhes do pedido</DialogDescription>
       </DialogHeader>
 
-      {order && (
+      {order ? (
         <div className="space-y-6">
           <Table>
             <TableBody>
@@ -121,6 +123,8 @@ export function OrderDetails({ orderId, open }: OrderDetailsProps) {
             </TableFooter>
           </Table>
         </div>
+      ) : (
+        <OrderDetailsSkeleton />
       )}
     </DialogContent>
   );
