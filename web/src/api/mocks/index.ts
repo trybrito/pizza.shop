86946edc71710739f9fcd5f09e0cdd78ebfe956a
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-02-04 20:02:49 -0300

    mock: approve + cancel + deliver + dispatch orders

--- web/src/api/mocks/index.ts
+++ web/src/api/mocks/index.ts
@@ -2,6 +2,10 @@ import { setupWorker } from "msw/browser";
 
 import { env } from "@/env";
 
+import { approveOrderMock } from "./approve-order-mock";
+import { cancelOrderMock } from "./cancel-order";
+import { deliverOrderMock } from "./deliver-order";
+import { dispatchOrderMock } from "./dispatch-order";
 import { getDailyRevenueInPeriodMock } from "./get-daily-revenue-in-period";
 import { getDayOrdersAmountMock } from "./get-day-orders-amount-mock";
 import { getManagedRestaurantMock } from "./get-managed-restaurant-mock";
@@ -30,6 +34,10 @@ export const worker = setupWorker(
   updateProfileMock,
   getOrdersMock,
   getOrderDetailsMock,
+  approveOrderMock,
+  cancelOrderMock,
+  deliverOrderMock,
+  dispatchOrderMock,
 );
 
 export async function enableMSW() {
